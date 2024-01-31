8967e829d6f8c0b6eef6c5df99d776fd0cc25ecd
xturnalgamer <william.cantrell@searcyschools.org>
2024-01-31 11:10:40 -0600

    man i overscoped

--- order.py
+++ order.py
@@ -1,8 +1,8 @@
-import pandas
+import pandas as pd
 
 order = [0,3]
-
-
+ptypes = pd.read_csv("data/titanic.csv")
+custchoices = ptypes[["Type"]]
 def onEnter():
     #runs when user enter the room
     return [1]
@@ -10,9 +10,12 @@ def update(input):
     global order
     
     match order[0]:
+        #counter
         case 0:
             pass
+        #thinking customer (roll rng and create order)
         case 1:
+            
             pass 
     return [1]
     
@@ -23,7 +26,10 @@ def draw_scene():
             if order[1] > 0:
                 print(f"there are {order[1]} customer(s) waiting.")
                 print("press \"e\" to take an order")
-                print("\003")
+            else:
+                print("there are no customers waiting")
+                
         case 1:
-            print(order)
+            print("The customer is thinking.")
+            print("(Press enter to continue.)")
 
